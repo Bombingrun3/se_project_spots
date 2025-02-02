2cb001cecc404fc0ea8ed7b467387fd6ce094315
Ryan <bombingrun3@gmail.com>
2025-02-01 23:57:42 -0500

    update Form Requirements

--- scripts/index.js
+++ scripts/index.js
@@ -175,3 +175,16 @@ function closeModalOnEsc(evt) {
 openedModal.addEventListener("keydown", () => {
   closeModalOnEsc(evt);
 });
+
+// config object
+
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(settings);
